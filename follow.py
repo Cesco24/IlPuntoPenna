3b3ca418b88e85a3cd0c26ed6706134a8fbd7697
GiordyZanna <zanellagiordano672@gmail.com>
2026-03-31 12:30:49 +0200

    bob

--- follow.py
+++ follow.py
@@ -1,3 +1,11 @@
 def somma(a,b):
     return a + b
+
+def saluta(nome):{
+    print (f"Ciao {nome} followa il.penna su instagram!!!")
+}
+
+def saluta(nome):{
+    print("Ciao" + nome + "followa il.penna su instagram!!!")
+}
 print("Bomber")
